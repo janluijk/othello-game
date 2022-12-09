3248df66ea77a1412228b4909c1df65dfb2678c0
janluijk <luijk.jan@gmail.com>
2022-12-09 12:58:29 +0100

    Eindelijk werkt zetten van waarde! Ritsen moet beter!

--- include/othellobord.h
+++ include/othellobord.h
@@ -3,6 +3,7 @@ struct Vec
     int x;
     int y;
 };
+
 class Bordvakje {
   public:
     char kleur;           //     7 0 1
@@ -11,29 +12,34 @@ class Bordvakje {
       kleur = '.';
       for (int i = 0; i < 8; i++)
       {
-        buren[i] = NULL;
+        buren[i] = nullptr;
       }
     }
 
 };//bordvakje
 
-
-
 class Othellobord {
   private:
+
     Bordvakje* ingang;
+    
+    // Variables
     int mapgrootte;
+
+    // Functions
+    Bordvakje* elementPtr(Vec);
   public:
+    
     // Functions
     Vec krijgCoordinaten();
     void afdrukken();
     void zetPositie();
     char krijgKleur();
-    Bordvakje* elementPtr(Vec);
     void ritsMap();
 
     // Constructor
     Othellobord() {
+      ritsMap();
       mapgrootte = 8;
     }
 };
